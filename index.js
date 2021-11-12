d1758e3f6a45dbbc413d5684c25a38b6db14fad8
Burt <bortsigan@users.noreply.github.com>
2021-11-12 22:47:54 +0800

    changes

--- index.js
+++ index.js
@@ -12,24 +12,31 @@ const mainPage  = "https://kb.createit.com";
 const email     = "developer@createit.com";
 const password  = "HkR9J#fq";
 
-const main_directory = 'Compilations';
+const main_directory = "Compilations";
+const windows_platform = "win32";
+
+const brand_materials_createit = "https://kb.createit.com/s/bivc8ufjq7oqncgnk1i0/2.-marketing/d/bj83ok7jq7otdq3t0bsg/1.-brand-materials-createit";
+const promotional_materials = "https://kb.createit.com/s/bivc8ufjq7oqncgnk1i0/2.-marketing/d/bj84du7jq7otdq3t0g70/3.-promotional-materials";
+const graphic_stock_createit = "https://kb.createit.com/s/bivc8ufjq7oqncgnk1i0/2.-marketing/d/bjdd5dfjq7ohb323d07g/4.-graphic-stock-createit";
+const graphic_stock_link365 = "https://kb.createit.com/s/bivc8ufjq7oqncgnk1i0/2.-marketing/d/bjdbcsvjq7ohb323d010/5.-graphic-stock-link365";
 
 (async () => {
+
     const browser   = await puppeteer.launch({ headless : false }); // { headless : false } open new page
     const pageOrg   = await browser.newPage();
     const page      = pageExtend(pageOrg);
+    const navigationPromise = page.waitForNavigation({waitUntil: "domcontentloaded"});
+
+
     await page.goto(mainPage);
 
     await login(page, email, password);
-
-    await page.goto(
-        "https://kb.createit.com/s/bivc8ufjq7oqncgnk1i0/2.-marketing/d/bj83ok7jq7otdq3t0bsg/1.-brand-materials-createit",
-        { waitUntil: 'load', timeout: 10000 }
-    );
-
+    await navigationPromise;
+    await page.goto(graphic_stock_createit, {waitUntil: 'load', timeout: 0 });
+    await navigationPromise;
     // wait for the appearance of a specific element in the Page
-    await page.waitForSelector('div.document-meta > div.document-heading > h1.name', {visible: true});
-    
+    //await page.waitForSelector('div.document-meta > div.document-heading > h1.name', {visible: true});
+    await page.waitForFunction("document.querySelector('.wysiwyg-table')");
     /**
      1. Check if page tile (h1 element) is already shown
      2. count number of tables existed in the page
@@ -37,122 +44,107 @@ const main_directory = 'Compilations';
      4. before proceeding to the next loop, check number of trs that has downloadables
      5. loop thru trs that has downloadables
      6. then proceed to the next loop of table
-     **/    
+     **/  
+
+    /**
+        Promotional Materials -> Gagdget Design [no wysiwyg-table class]
+    **/
 
     let page_title      = await page.jQuery('div.document-meta > div.document-heading > h1.name').html();
-    let num_of_tables   = await page.jQuery(`table.wysiwyg-table`);
+    let num_of_tables   = await page.jQuery(`table`);
 
     for (let table_index = 0; table_index < num_of_tables.length; table_index++) {
-        let number_of_rows  = 0;
+        let number_of_downloadables  = 0;
         let dir_path        = "";
+        let fs_mkdir        = "";
         let title           = await getGroupTitle(page, table_index);
 
         if (title.toString().indexOf("/") > 0) {
-            title = title.toString().replace(/\//ig, 'and');
+            title = title.toString().replace(/\//ig, '&');
         }
 
         if (title.toString().indexOf("&") > 0) {
             title = title.toString().replace(/&amp;/g, '&');
         }
 
-        dir_path = `./${main_directory}/${page_title}/${title}`;
+        console.log(`\n------ TABLE NAME: ${title} ---------`);
+
+        dir_path = path.resolve(`./${main_directory}/${page_title}/${title}`);
 
         if (!fs.existsSync(dir_path)) {
             // create path if not existent
-            await fsPromises.mkdir( __dirname + dir_path, { recursive: true });
+            await fsPromises.mkdir(dir_path, {recursive: true});
         }
 
         // set download path
         await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: dir_path});
 
-        number_of_rows  = await page.jQuery(`table.wysiwyg-table:eq(${table_index}) > tbody > tr`);
+        number_of_downloadables = await page
+                                        .jQuery(`table:eq(${table_index}) > tbody > tr > td`)
+                                        .find(`a[data-link-type='file']`);
         
         // loop thru the trs of each table
-        for (let table_rows_index = 1; table_rows_index <= number_of_rows.length; table_rows_index++) {
-        
+        for (let index = 0; index < number_of_downloadables.length; index++) {
+            
+            // check downloadables element inside td of each table
+            let element = await page
+                                    .jQuery(`table:eq(${table_index}) > tbody > tr > td`)
+                                    .find(`a[data-link-type='file']`)
+                                    .eq(index);
+
+            if (typeof element[0] !== 'undefined') {
+                element[0].click();
+
+                let html = await page
+                                    .jQuery(`table:eq(${table_index}) > tbody > tr > td`)
+                                    .find(`a[data-link-type='file']`)
+                                    .eq(index)
+                                    .html();
+                
+                console.log("\nitem #: (" + index + ") item name : " + html);
+            }
             await sleep(3000);
-            
-            let num_of_br = await page.jQuery(`table.wysiwyg-table:eq(${table_index}) > tbody > tr:nth-child(${table_rows_index}) > td`).find('br');
-
-            // remove br if it exist in the corresponding td element
-            // if (num_of_br.length > 0) { await page.jQuery(`table.wysiwyg-table:eq(${table_index}) > tbody > tr:nth-child(${table_rows_index}) > td`).find('br').remove(); }
-            
-            // check element downloadables inside td of each table
-            let element_downloadables = await page
-                                                .jQuery(`table.wysiwyg-table:eq(${table_index}) > tbody > tr:nth-child(${table_rows_index}) > td`)
-                                                .find(`a[data-link-type='file']`);
-
-            // check if one td has multiple downloadables
-            if (element_downloadables.length > 1) {
-            
-                // loop thru each downloadables 
-                for (let elem_len = 0; elem_len < element_downloadables.length; elem_len++) {
-                    await sleep(3000);
-
-                    let element_download = await page
-                                                    .jQuery(`table.wysiwyg-table:eq(${table_index}) > tbody > tr:nth-child(${table_rows_index}) > td`)
-                                                    .find("a[data-link-type='file']")
-                                                    .eq(elem_len);
-                    
-                    if (typeof element_download[0] !== 'undefined') {
-                        element_download[0].click();
-                    }
-                }
-
-            } else {
-                if (typeof element_downloadables[0] !== 'undefined') {
-
-                    console.log(await page.jQuery(`table.wysiwyg-table:eq(${table_index}) > tbody > tr:nth-child(${table_rows_index}) > td`).find("a[data-link-type='file']").html() + "\n");
-                    element_downloadables[0].click();
-                }
-            } 
         }
         await sleep(3000);
     }
 
-    
-/*    await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: './TestFolder'});
 
-    let sample_index =2;
-    let number_of_rows  = await page.jQuery(`table.wysiwyg-table:eq(${sample_index}) > tbody > tr`);
+/*    await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: path.resolve(`./TestFolder`)});
 
-    for (let table_rows = 1; table_rows <= number_of_rows.length; table_rows++) {
-        await sleep(3000);
 
-        let element_downloadables = await page
-                                            .jQuery(`table.wysiwyg-table:eq(${sample_index}) > tbody > tr:nth-child(${table_rows}) > td`)
-                                            .find(`a[data-link-type='file']`);
+    let sample_index = 3;
+    let number_of_downloadables  = await page
+                                            .jQuery(`table`)
+                                            .eq(sample_index)
+                                            .find('tbody > tr > td')
+                                            .find('a');
 
-        let num_of_br = await page.jQuery(`table.wysiwyg-table:eq(${sample_index}) > tbody > tr:nth-child(${table_rows}) > td`).find('br'); 
-    
-        // if (num_of_br.length > 0) { await page.jQuery(`table.wysiwyg-table:eq(${sample_index}) > tbody > tr:nth-child(${table_rows}) > td`).find('br').replaceWith(" "); } 
+    console.log(number_of_downloadables.length);
+    for (let i = 0; i < number_of_downloadables.length; i++) {
+        console.log(i);
 
-        if (element_downloadables.length > 1) {
-            
-            for (let elem_len = 0; elem_len < element_downloadables.length; elem_len++) {
-                await sleep(3000);
+        let element = await page.jQuery(`table`).eq(sample_index).find(`a[data-link-type='file']`).eq(i);
+        element[0].click();
 
-                let element_download =  await page
-                                                .jQuery(`table.wysiwyg-table:eq(${sample_index}) > tbody > tr:nth-child(${table_rows}) td`)
-                                                .find("a[data-link-type='file']")
-                                                .eq(elem_len);
-            
+        console.log(await page.jQuery(`table`).eq(sample_index).find(`a[data-link-type='file']`).eq(i).html());
+        
+        sleep(5000);
+    }*/
 
-                if (typeof element_download[0] !== 'undefined') {
-                    element_download[0].click();
-                }
-            }
+    /*
+    let sample_index = 0;
 
-        } else {
+    for (let sample_index = 0; sample_index < 2; sample_index++) {
+        console.log("\ntable: " + sample_index);
 
-            if (typeof element_downloadables[0] !== 'undefined') {
-                element_downloadables[0].click();
+        let number_of_downloadables = await page.jQuery(`table:eq(${sample_index}) > tbody > tr > td`).find(`a[data-link-type='file']`);
+        for (let index = 0; index < number_of_downloadables.length; index++) {
+            await sleep(3000);
 
-                console.log(await page
-                        .jQuery(`table.wysiwyg-table:eq(${sample_index}) > tbody > tr:nth-child(${table_rows}) > td > a[data-link-type='file']`)
-                        .html() + "\n"
-                );
-            }
+            let element = await page.jQuery(`table:eq(${sample_index}) > tbody > tr > td`).find(`a[data-link-type='file']`).eq(index);
+            let html = await page.jQuery(`table:eq(${sample_index}) > tbody > tr > td > a[data-link-type='file']`).eq(index).html();
+            console.log("\nitem #: (" + index + ") item name : " + html);
+            element[0].click();
         }
     }*/
 
@@ -200,7 +192,8 @@ async function autoScroll(page){
 let getGroupTitle = (page, index) => {
 
     let title = page
-            .jQuery(`table.wysiwyg-table:eq(${index})`)
+            .jQuery(`table`)
+            .eq(index)
             .parent()
             .parent()
             .parent()
--- test.js
+++ test.js
@@ -5,53 +5,135 @@ const { pageExtend }    = require('puppeteer-jquery');
   const browser = await puppeteer.launch({headless: false});
   const pageOrg = await browser.newPage();
   const page      = pageExtend(pageOrg);
-  page.setContent(`<table class="wysiwyg-table" style="width: 99.9071%;">
+  page.setContent(`<table class="wysiwyg-table" style="width: 100%; height: 680px;">
 <thead>
-<tr style="background-color: #f7f3f0; text-align: center;">
-<td style="width: 33.3333%;"><b>File</b></td>
-<td style="width: 33.3333%;"><b>Comment</b></td>
-<td style="width: 33.3333%;"><b>Last Update</b></td>
+<tr style="height: 32px;">
+<th style="height: 32px; width: 25.0464%;">Name</th>
+<th style="height: 32px; width: 24.9536%;">File</th>
+<th style="height: 32px; width: 24.9536%;">Comment</th>
+<th style="height: 32px; width: 24.9536%;">Last Update</th>
 </tr>
 </thead>
 <tbody>
-<tr style="border-color: #e87722;">
-<td style="width: 33.3333%;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/bj83vavjq7otdq3t0bu0" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="bj840dfjq7otdq3t0c1g" data-link-target-document-id="bj83ok7jq7otdq3t0bsg" data-link-target-id="bj83vavjq7otdq3t0bu0" data-link-type="file">createit-logo.zip</a></td>
-<td style="width: 33.3333%;">standard logos set - multicolored</td>
-<td style="width: 33.3333%;">04.04.2019</td>
+<tr style="height: 32px;">
+<td style="width: 25.0464%; height: 32px;">Company in a glance brochure</td>
+<td style="width: 24.9536%; height: 32px;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/c04r79l8gmvrikka6uig" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="c04r8c58gmvrikka71o0" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="c04r79l8gmvrikka6uig" data-link-type="file">createIT at a glance january_2021_2.pdf</a></td>
+<td style="width: 24.9536%; height: 32px;">createIT introduction, phone number updated</td>
+<td style="width: 24.9536%; height: 32px;">21.01.2021</td>
 </tr>
-<tr style="border-color: #e87722;">
-<td style="width: 33.3333%;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/c13045l8gmvsiqcnisng" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="c1304lt8gmvsiqcnita0" data-link-target-document-id="bj83ok7jq7otdq3t0bsg" data-link-target-id="c13045l8gmvsiqcnisng" data-link-type="file">createit-logo-hi-res.zip</a></td>
-<td style="width: 33.3333%;">hi resolution logos set</td>
-<td style="width: 33.3333%;">08.03.2021</td>
+<tr style="height: 53px;">
+<td style="width: 25.0464%; height: 53px;">Outsourcing services brochure</td>
+<td style="width: 24.9536%; height: 53px;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/bkhii6njq7osksebff2g" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="bkhii9vjq7osksebffm0" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="bkhii6njq7osksebff2g" data-link-type="file">createIT-Developer Outsourcing updated july19.pdf</a></td>
+<td style="width: 24.9536%; height: 53px;">outsourcing/bod leasing services, phone number updated</td>
+<td style="width: 24.9536%; height: 53px;">29.05.2019</td>
+</tr>
+<tr style="height: 53px;">
+<td style="width: 25.0464%; height: 53px;">Portfolio brochure</td>
+<td style="width: 24.9536%; height: 53px;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/c04r78l8gmvrikka6ui0" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="c04r8kl8gmvrikka72k0" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="c04r78l8gmvrikka6ui0" data-link-type="file">Portfolio-onepage updated 20_01_2021.pdf</a></td>
+<td style="width: 24.9536%; height: 53px;">portfolio onepager, pdf</td>
+<td style="width: 24.9536%; height: 53px;">21.01.2021</td>
+</tr>
+<tr style="height: 53px;">
+<td style="width: 25.0464%; height: 276px;" rowspan="6">SEO&nbsp;</td>
+<td style="width: 24.9536%; height: 53px;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/bkhhhs7jq7osksebfau0" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="bkhhid7jq7osksebfbf0" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="bkhhhs7jq7osksebfau0" data-link-type="file">SEO.png</a></td>
+<td style="width: 24.9536%; height: 53px;">Ongoing fine tuning ans results monitoring diagram</td>
+<td style="width: 24.9536%; height: 53px;">08.07.2019</td>
+</tr>
+<tr style="height: 53px;">
+<td style="width: 24.9536%; height: 53px;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/bkhhi1fjq7osksebfavg" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="bkhhignjq7osksebfceg" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="bkhhi1fjq7osksebfavg" data-link-type="file">seo.psd</a></td>
+<td style="width: 24.9536%; height: 53px;">Ongoing fine tuning ans results monitoring diagram</td>
+<td style="width: 24.9536%; height: 53px;">08.07.2019</td>
+</tr>
+<tr style="height: 32px;">
+<td style="width: 24.9536%; height: 32px;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/bkhhhrvjq7osksebfatg" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="bkhhij7jq7osksebfcug" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="bkhhhrvjq7osksebfatg" data-link-type="file">SEO2.png</a></td>
+<td style="width: 24.9536%; height: 32px;">4 areas of SEO</td>
+<td style="width: 24.9536%; height: 32px;">08.07.2019</td>
+</tr>
+<tr style="height: 53px;">
+<td style="width: 24.9536%; height: 53px;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/bkhhi1fjq7osksebfav0" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="bkhhin7jq7osksebfdt0" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="bkhhi1fjq7osksebfav0" data-link-type="file">seo-pl.psd</a></td>
+<td style="width: 24.9536%; height: 53px;">Ongoing fine tuning ans results monitoring diagram PL</td>
+<td style="width: 24.9536%; height: 53px;">08.07.2019</td>
+</tr>
+<tr style="height: 53px;">
+<td style="width: 24.9536%; height: 53px;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/bkhhhs7jq7osksebfaug" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="bkhhip7jq7osksebfek0" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="bkhhhs7jq7osksebfaug" data-link-type="file">seo-pl.png</a></td>
+<td style="width: 24.9536%; height: 53px;">Ongoing fine tuning ans results monitoring diagram PL</td>
+<td style="width: 24.9536%; height: 53px;">08.07.2019</td>
+</tr>
+<tr style="height: 32px;">
+<td style="width: 24.9536%; height: 32px;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/brh2v4vjq7osjogpkr0g" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="brh2v7fjq7osjogpkra0" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="brh2v4vjq7osjogpkr0g" data-link-type="file">Oferta SEO, Remarketing GA_2020_v1.docx</a></td>
+<td style="width: 24.9536%; height: 32px;">example of SEO offer</td>
+<td style="width: 24.9536%; height: 32px;">11.06.2020</td>
+</tr>
+<tr style="height: 32px;">
+<td style="width: 25.0464%; height: 64px;" rowspan="2">Project flow</td>
+<td style="width: 24.9536%; height: 32px;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/bm8tfcvjq7osln7bnrog" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="bm8tfjvjq7osln7bnsag" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="bm8tfcvjq7osln7bnrog" data-link-type="file">createIT-project-flow_web_updated.pdf</a></td>
+<td style="width: 24.9536%; height: 32px;">project flow borchure, pdf</td>
+<td style="width: 24.9536%; height: 32px;">30.09.2019</td>
+</tr>
+<tr style="height: 32px;">
+<td style="width: 24.9536%; height: 32px;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/bkrflqfjq7ondiu7747g" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="bkrfns7jq7ondiu774s0" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="bkrflqfjq7ondiu7747g" data-link-type="file">work flow PL.pdf</a></td>
+<td style="width: 24.9536%; height: 32px;">project flow scheme - PL</td>
+<td style="width: 24.9536%; height: 32px;">23.07.2019</td>
+</tr>
+<tr style="height: 53px;">
+<td style="width: 25.0464%; height: 53px;">GDPR adaptations</td>
+<td style="width: 24.9536%; height: 53px;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/c4mc1od8gmvshmu2ujh0" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="c4mc1ul8gmvshmu2ujsg" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="c4mc1od8gmvshmu2ujh0" data-link-type="file">GDPR-adaptations-offer_21.pdf</a></td>
+<td style="width: 24.9536%; height: 53px;">gdpr offer</td>
+<td style="width: 24.9536%; height: 53px;">30.08.2021</td>
+</tr>
+<tr style="height: 32px;">
+<td style="width: 25.0464%; height: 32px;" rowspan="2">createIT in numbers<br><br></td>
+<td style="width: 24.9536%; height: 32px;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/c0bvri58gmvrikka7jr0" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="c0bvrrd8gmvrikka7k80" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="c0bvri58gmvrikka7jr0" data-link-type="file">PL_v1.png</a></td>
+<td style="width: 24.9536%; height: 32px;">createIt in numbers PL</td>
+<td style="width: 24.9536%; height: 32px;">01.02.2021</td>
+</tr>
+<tr style="height: 32px;">
+<td style="width: 24.9536%; height: 32px;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/c0bvri58gmvrikka7jqg" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="c0bvs758gmvrikka7lag" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="c0bvri58gmvrikka7jqg" data-link-type="file">ENG_v1.png</a></td>
+<td style="width: 24.9536%; height: 32px;">createIT in number EN</td>
+<td style="width: 24.9536%; height: 32px;">01.02.2021</td>
 </tr>
 <tr>
-<td style="width: 33.3333%;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/c13040l8gmvsiqcnisn0" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="c1305el8gmvsiqcnivc0" data-link-target-document-id="bj83ok7jq7otdq3t0bsg" data-link-target-id="c13040l8gmvsiqcnisn0" data-link-type="file">createit-logo-colored.ai</a></td>
-<td style="width: 33.3333%;">colored logo: pdf + ai</td>
-<td style="width: 33.3333%;">08.03.2021</td>
+<td style="width: 25.0464%;" rowspan="2">createIT services</td>
+<td style="width: 24.9536%;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/bm8uannjq7osln7bnsi0" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="bm8uapvjq7osln7bnt60" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="bm8uannjq7osln7bnsi0" data-link-type="file">infografika_pl.png</a></td>
+<td style="width: 24.9536%;">services infographic PL</td>
+<td style="width: 24.9536%;">30.09.2019</td>
 </tr>
 <tr>
-<td style="width: 33.3333%;">
-<p><a href="/api/public/attachment/biqckdvjq7orh4iripf0/c4f1sd58gmvshmu2ufv0" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="c4f1sdt8gmvshmu2ugr0" data-link-target-document-id="bj83ok7jq7otdq3t0bsg" data-link-target-id="c4f1sd58gmvshmu2ufv0" data-link-type="file">logo-createIT-confidential (1).png</a></p>
-<br><br>
-<p><a href="/api/public/attachment/biqckdvjq7orh4iripf0/c4f1sd58gmvshmu2ufug" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="c4f1sq58gmvshmu2ui0g" data-link-target-document-id="bj83ok7jq7otdq3t0bsg" data-link-target-id="c4f1sd58gmvshmu2ufug" data-link-type="file">logo-createIT-confidential.svg</a></p>
-</td>
-<td style="width: 33.3333%;">createIT logo with confidential badge</td>
-<td style="width: 33.3333%;">18.08.2021</td>
+<td style="width: 24.9536%;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/bm8uannjq7osln7bnshg" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="bm8ub37jq7osln7bnttg" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="bm8uannjq7osln7bnshg" data-link-type="file">infografika_eng.png</a></td>
+<td style="width: 24.9536%;">services infographic EN</td>
+<td style="width: 24.9536%;">30.09.2019</td>
 </tr>
 <tr>
-<td style="width: 33.3333%;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/c04rf0t8gmvrikka76jg" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="c04rf2d8gmvrikka771g" data-link-target-document-id="bj83ok7jq7otdq3t0bsg" data-link-target-id="c04rf0t8gmvrikka76jg" data-link-type="file">createIT_3d.zip</a></td>
-<td style="width: 33.3333%;">createIT 3D logo</td>
-<td style="width: 33.3333%;">21.01.2021</td>
+<td style="width: 25.0464%;">createIT for FinTech&nbsp;</td>
+<td style="width: 24.9536%;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/bnkgrcvjq7osj4mmojfg" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="bnkgrd7jq7osj4mmok0g" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="bnkgrcvjq7osj4mmojfg" data-link-type="file">createIT-for-fintech-companies.pdf</a></td>
+<td style="width: 24.9536%;">createIT for fintech companies brochure</td>
+<td style="width: 24.9536%;">05.12.2019</td>
+</tr>
+<tr>
+<td style="width: 25.0464%;">OXID Folder</td>
+<td style="width: 24.9536%;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/c4mc1od8gmvshmu2ujhg" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="c4mc29t8gmvshmu2ul9g" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="c4mc1od8gmvshmu2ujhg" data-link-type="file">Oxid_brochure_createIT_21.pdf</a></td>
+<td style="width: 24.9536%;">nearshoring / programming services / oxid</td>
+<td style="width: 24.9536%;">30.08.2021</td>
+</tr>
+<tr>
+<td style="width: 25.0464%;">WordPress brochure</td>
+<td style="width: 24.9536%;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/c0il0vt8gmvrikka7tk0" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="c0il16l8gmvrikka7urg" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="c0il0vt8gmvrikka7tk0" data-link-type="file">wordpress_january 2021 upd.pdf</a></td>
+<td style="width: 24.9536%;">WordPress / offer</td>
+<td style="width: 24.9536%;">11.02.2021</td>
+</tr>
+<tr>
+<td style="width: 25.0464%;">Digital Marketing</td>
+<td style="width: 24.9536%;"><a href="/api/public/attachment/biqckdvjq7orh4iripf0/c2v3bd58gmvslgg398kg" data-documize="true" data-link-space-id="bivc8ufjq7oqncgnk1i0" data-link-id="c2v3bkt8gmvslgg399jg" data-link-target-document-id="bj84du7jq7otdq3t0g70" data-link-target-id="c2v3bd58gmvslgg398kg" data-link-type="file">digital marketing_2021.pdf</a></td>
+<td style="width: 24.9536%;">Digital Marketing - onepage&nbsp;</td>
+<td style="width: 24.9536%;">07.06.2021</td>
 </tr>
 </tbody>
 </table>`
 );
  
 
-  const trs = await page.jQuery('table > tbody > tr > td').find("a[data-link-type='file']").eq(4).html();
-
-  console.log(trs);
-
+let number_of_downloadables  = await page.jQuery(`table > tbody > tr > td`).find('a').eq(4);
+console.log(number_of_downloadables[0].click());
 
   // let element = await page.jQuery('table.wysiwyg-table:eq(2)) > tbody > tr:nth-child(2) > td').find("br").remove();
   // console.log(element);
